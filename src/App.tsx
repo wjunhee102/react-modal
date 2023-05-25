c55cb41a01761daf70925d630f3a0da23ce4beeb
wjunhee102 <wjunhee102@gmail.com>
2023-05-25 13:07:53 +0900

    예제

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { modalController } from './modal';
+import { ModalRegistrator, modalController } from './modal';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,7 +21,25 @@ function App() {
         >
           Learn React
         </a>
-        <button onClick={() => modalController.open("modal1")}>모달 열기</button>
+        <button onClick={() => modalController.open("modal1", { content: "모달 1"})}>모달 열기</button>
+        <button onClick={() => modalController.open("modal2", { content: "모달 2", position: "bottom" })}>모달2 열기</button>
+        <button onClick={() => setIsOpen(true)}>모달2 열기</button>
+        <ModalRegistrator name="modal2" open={isOpen} setOpen={(setIsOpen)}>
+          {
+            ({ closeModal, confirmModalCallback, confirmModalCallbackProps, content }) => (
+              <div className="w-[300px] h-[100px] bg-white shadow-sm shadow-slate-50">
+                <div>
+                  {content}
+                </div>
+                <div className="flex justify-between items-center h-full px-4">
+                  <button onClick={() => closeModal()}>Close</button>
+                  <button onClick={() => closeModal(confirmModalCallback, confirmModalCallbackProps)}>Confirm</button>
+                </div>
+              </div>
+            )
+          }
+          
+        </ModalRegistrator>
       </header>
     </div>
   );
