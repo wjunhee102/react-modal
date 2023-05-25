41c9a22363c0512091b425da52850aaa44e65dd0
wjunhee102 <wjunhee102@gmail.com>
2023-05-25 13:03:27 +0900

    [feat] 예제

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { modalController } from './modal';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={() => modalController.open("modal1")}>모달 열기</button>
       </header>
     </div>
   );
--- src/components/ModalContainer.tsx
+++ src/components/ModalContainer.tsx
@@ -1,19 +1,40 @@
-import { ModalComponent, ModalDispatcher } from "../modal";
+import { ModalComponent, ModalComponentMeta, ModalDispatcher } from "../modal";
 
-const Modal1: ModalComponent = ({ }) => {
+const Modal1: ModalComponent = ({ closeModal, confirmModalCallback, confirmModalCallbackProps, content }) => {
+  
+  const onClickClose = () => {
+    closeModal();
+  }
+
+  const onClickConfirm = () => {
+    closeModal(confirmModalCallback, confirmModalCallbackProps);
+  }
+  
   return (
-    <div className="">
-
+    <div className="w-[300px] h-[100px] bg-white shadow-sm shadow-slate-50">
+      <div>
+        {content}
+      </div>
+      <div className="flex justify-between items-center h-full px-4">
+        <button onClick={onClickClose}>Close</button>
+        <button onClick={onClickConfirm}>Confirm</button>
+      </div>
     </div>
   )
 }
 
-const modalComponentMeta = {
-
-}
+const modalComponentMetaList: ModalComponentMeta[] = [
+  {
+    name: "modal1",
+    component: Modal1,
+    defaultOptions: {
+      duration: 400,
+    }
+  }
+]
 
 const ModalContainer = () => {
-  return <ModalDispatcher modalComponentMeta=[{} />;
+  return <ModalDispatcher modalComponentMeta={modalComponentMetaList} />;
 }
 
 export default ModalContainer;
